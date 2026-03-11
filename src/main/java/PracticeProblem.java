8c2c623ff8266235d9672db0c0bd2bb2bf89d2c0
BensonZhengFromACCI <343804126@tdsb.ca>
2026-03-11 15:42:17 +0000

    thanks mr kalisz

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,27 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
+public static boolean isEven(int num1) {
+return (num1 % 2 == 0);
+}
+
+public static boolean isOdd(int num1) {
+return (num1 % 2 != 0);
+}
+
+public static boolean isPositive(int num1) {
+return (num1 > 0);
+}
+
+public static boolean isNegative(int num1) {
+return (num1 < 0);
+}
+
+public static int combinedLength(String word1, String word2) {
+return (word1.length() + word2.length());
 
 }
+	}
+
+
